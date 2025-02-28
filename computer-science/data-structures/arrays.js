810c1840a43f1b4b2ac88bc885815192c878f56b
AlexandrShum <75227139+AlexandrShum@users.noreply.github.com>
2025-02-28 14:07:39 +0200

    Create arrays.js

---
+++ computer-science/data-structures/arrays.js
@@ -0,0 +1,12 @@
+// Array of strings in JS
+const array = ["Corey Taylor", "Mick Thomson", "Paul Gray"];
+
+console.log(array); // This command will show our array to terminal ["Corey Taylor", "Mick Thomson", "Paul Gray"]
+
+// We can find element using index
+const secondElement = array[1];
+console.log(secondElement); // Will show in console second value of array -> "Mick Thomson"
+
+// For adding element to end of list we should use push method
+array.push("Joey Jordison");
+console.log(array); // Now we will see 4 elements in our array -> ["Corey Taylor", "Mick Thomson", "Paul Gray", "Joey Jordison"]
